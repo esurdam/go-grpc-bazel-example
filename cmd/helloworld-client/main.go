9372c3ad0ed97f809135464707d6e3f161255bb4
Evan Surdam <evan@adgreetz.com>
2022-06-26 02:47:28 -0700

    cmd: Add `helloworld-client`

---
+++ cmd/helloworld-client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	pb "github.com/AdGreetz/go-grpc-bazel-example/pb/helloworld"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var serverAddr = flag.String("server-addr", ":9000", "grpc server address")
+	var name = flag.String("name", "", "name to greet")
+	flag.Parse()
+
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	cli := pb.NewGreeterClient(conn)
+	reply, err := cli.SayHello(context.Background(), &pb.HelloRequest{
+		Name: *name,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(reply)
+}
