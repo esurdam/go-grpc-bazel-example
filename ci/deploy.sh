7a949cf12c1d574edf171368d385a167e2d5c418
Evan Surdam <esurdam@gmail.com>
2025-05-28 04:06:06 -0700

    Bump bazel deps and clean builds and deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # This script is used to deploy services to k8s
+# It will:
+# 1. Query all push rules
+# 2. For each push rule, it will:
+#    a. Check if the yaml file exists
+#    b. Run the push rule to get the image tag 
+#    c. Get the version from the stamped file
+#    d. Replace the image tag and version in the yaml file
+#    e. Apply the yaml file to k8s
 
 env GO111MODULE=on
 
@@ -7,12 +15,26 @@ set -e
 set -u
 set -x
 
+TOOLCHAIN="@rules_go//go/toolchain:linux_amd64" # TODO: Make this configurable
+CPU="k8"  # k8 is the default cpu for linux
+
+process_service() {
+  local i=$1
+  local root="${i/\/\//}" # remove the leading //
+  local core="${root/:push/}" # remove the trailing :push
+  # check if yaml exists
+  if [ ! -f "ci/${core}.yaml" ]; then
+    echo "yaml file not found: ci/${core}.yaml"
+    return 0
+  fi
+  local IMAGE_TAG=$(bazel run --platforms=$TOOLCHAIN --cpu=$CPU "$i")
+  local VERSION=$(cat "$(bazel cquery --output=files "${i/:push/:stamped}")")
+  # replace the image tag and version in the yaml file
+  local template=$(cat "ci/${core}.yaml" | sed "s#{{IMAGE_TAG}}#$IMAGE_TAG#g" | sed "s/{{VERSION}}/$VERSION/g")
+  echo "$template"
+}
+
 # Queries for all push rules and deploys them
 for i in $(bazel query 'kind(".push rule", //...)'); do
-  IMAGE_TAG=$(bazel run --platforms=@rules_go//go/toolchain:linux_amd64 --cpu=k8 "$i")
-  VERSION=$(cat "$(bazel cquery --output=files "${i/:push/:stamped}")")
-  root="${i/\/\//}" # remove the leading //
-  core="${root/:push/}" # remove the trailing :push
-  template=$(cat "ci/${core}.yaml" | sed "s#{{IMAGE_TAG}}#$IMAGE_TAG#g" | sed "s/{{VERSION}}/$VERSION/g")
-  echo "$template"
+  process_service "$i"
 done
